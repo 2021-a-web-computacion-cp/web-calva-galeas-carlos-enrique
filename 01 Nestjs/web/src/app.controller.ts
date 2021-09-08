d1799f47928b8c6b6266ff411f1a330e12a3318c
Carlos Calva <carlos.calva@epn.edu.ec>
2021-09-08 18:52:46 -0500

    Cookies

--- 01 Nestjs/web/src/app.controller.ts
+++ 01 Nestjs/web/src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  InternalServerErrorException,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +17,59 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  // import {Controller, Get, HttpCode} from '@nestjs/common';
+  @Get('texto')
+  @HttpCode(200)
+  holaTexto(): string {
+    return 'HOLA TEXTO';
+  }
+  @Get('html')
+  @HttpCode(201)
+  holaHTML(): string {
+    return '<h1>Hola HTML</h1> <button>Click</button>';
+  }
+  @Get('json')
+  @HttpCode(200)
+  holaJSON(): string {
+    return '{mensaje: "Hola json" }';
+  }
+
+  @Get('bad-request')
+  badRequest() {
+    throw new BadRequestException();
+  }
+
+  @Get('internal-error')
+  internalError() {
+    throw new InternalServerErrorException();
+  }
+
+  @Get('setear-cookie-insegura')
+  setearCookieInsegura(
+    @Req() req, //  request - PETICION
+    @Res() res, //  response - RESPUESTA
+  ) {
+    res.cookie(
+      'galletaInsegura', // nombre
+      'Tengo hambre', // valor
+    );
+    res.cookie(
+      'galletaSegura', // nombre
+      'Web :3', // valor
+      {
+        secure: true,
+      },
+    );
+    res.send('ok'); // return de antes
+  }
+
+  @Get('mostrar-cookies')
+  mostrarCookies(@Req() req) {
+    const mensaje = {
+      sinFirmar: req.cookies,
+      firmadas: req.signedCookies,
+    };
+    return mensaje;
+  }
 }
+// npm i cookie-parser express-session session-file-store
